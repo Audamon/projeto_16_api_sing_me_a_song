a33fb9506525c4b325c1dc2c6ad475372fdb7a2f
Matheus Galdino <matheus@respondeai.com.br>
2021-12-07 12:49:42 -0300

    preparando o review

--- src/Services/recommendationsTop.js
+++
@@ -1,7 +0,0 @@
-import * as recommendationsTopRepository from '../Repositories/recommendationsTopRepository.js';
-
-async function recommendationsTop({ amount }) {
-  const recommendations = await recommendationsTopRepository.recommendationsTop({ amount });
-  return recommendations;
-}
-export { recommendationsTop };
